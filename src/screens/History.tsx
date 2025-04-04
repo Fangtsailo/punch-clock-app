bf81c1fb08f63e7223f89febfa33212e425e734a
Adam Lo <spm770311@gmail.com>
2025-04-04 18:24:15 +0800

    history ui

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,18 +1,28 @@
 import { useContext } from 'react';
-import { View, Text } from 'react-native';
-import { PunchHistoryListContext } from '@/context/PunchHistoryList';
+import { View, Text, FlatList } from 'react-native';
+import {
+  PunchHistory,
+  PunchHistoryListContext,
+} from '@/context/PunchHistoryList';
 export default function HistoryScreen(): JSX.Element {
   const { contextPunchHistoryList: punchHistoryList } = useContext(
     PunchHistoryListContext
   );
+  const punchHistoryItem = (record: PunchHistory, index: number) => {
+    return (
+      <View key={index} className="flex-row justify-between">
+        <Text className="text-white text-base mb-4">{record.date}</Text>
+        <Text className="text-white text-base mb-4">{record.time}</Text>
+      </View>
+    );
+  };
   return (
     <View className="flex-1 bg-gray-900 p-5">
       <Text className="text-2xl font-bold text-white mb-4">打卡歷史</Text>
-      {punchHistoryList.map((record, index) => (
-        <Text key={index} className="text-white text-base mb-1">
-          {record.date} - {record.time}
-        </Text>
-      ))}
+      <FlatList
+        data={punchHistoryList}
+        renderItem={({ item, index }) => punchHistoryItem(item, index)}
+      />
     </View>
   );
 }
